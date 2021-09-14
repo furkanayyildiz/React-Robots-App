0fd90cada32087bd1b5e9e557f4803e7818c57e0
FURKAN <furkan199905@gmail.com>
2021-09-14 17:54:43 +0300

    Scroll added

--- src/App.js
+++ src/App.js
@@ -3,14 +3,24 @@ import CardList from "./CardList";
 import { robots } from "./robots";
 import SearchBox from "./SearchBox";
 import "./App.css";
+import Scroll from "./Scroll";
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      robots: robots,
+      robots: [],
       searchfield: "",
     };
+  }
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => {
+        return response.json();
+      })
+      .then((users) => {
+        this.setState({ robots: users });
+      });
   }
   onSearchChange = (event) => {
     this.setState({ searchfield: event.target.value });
@@ -25,7 +35,9 @@ class App extends Component {
       <div className="tc">
         <h1 className="f1"> RoboFriends</h1>
         <SearchBox searchChange={this.onSearchChange} />
-        <CardList robots={filteredRobots} />
+        <Scroll>
+          <CardList robots={filteredRobots} />
+        </Scroll>
       </div>
     );
   }
